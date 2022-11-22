809640d9c41ebbbd0857a2e03514ce3ac16dbb24
IJin Yun <483759@naver.com>
2022-11-22 22:53:43 +0900

    #1 springboot application server initialize

---
+++ happy-chat/src/test/kotlin/com/example/happychat/HappyChatApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.happychat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HappyChatApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
